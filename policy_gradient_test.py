7bd051d73e3842d675a6d7f05a00da88b381ab8c
shincling <shijing609@163.com>
2016-09-19 11:05:38 +0800

    finish mu function

--- policy_gradient_test.py
+++ policy_gradient_test.py
@@ -8,3 +8,12 @@ def theta_policy(n_states,n_actions):
 
 theta=theta_policy(5,2)
 print theta
+
+def mu_policy(theta):
+    '''mu ---　softmax policy'''
+    n_states,n_actions=theta.shape
+    mu=np.zeros((n_states,n_actions))
+    for state in range(0,n_states):
+        max_theta=np.max(theta[state])
+        mu[state]=np.exp(theta[state]-max_theta)/np.sum(np.exp(theta[state]-max_theta))
+    return mu
