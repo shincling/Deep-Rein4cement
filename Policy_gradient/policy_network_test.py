50152395ab22e11aa6451d0a3196d1e23571f87f
shincling <shijing609@163.com>
2016-09-21 12:13:11 +0800

    add some metric and add a layer

--- Policy_gradient/policy_network_test.py
+++ Policy_gradient/policy_network_test.py
@@ -5,8 +5,8 @@ import theano
 import theano.tensor as T
 from sklearn.preprocessing import LabelBinarizer,label_binarize
 
-def get_dataset():
-    x=np.random.random((10000,10))
+def get_dataset(dimention):
+    x=np.random.random((10000,dimention))
     y=np.zeros((10000))
     for idx,i in enumerate(x):
         if 0.3<i[0]<0.7:
@@ -16,7 +16,8 @@ def get_dataset():
     return x,y
 
 
-xx,yy=get_dataset()
+dimention=10
+xx,yy=get_dataset(dimention)
 yy=np.int32(yy)
 x=T.matrix('x')
 y=T.imatrix('y')
@@ -24,7 +25,6 @@ x1=T.vector('x1')
 x2=T.matrix('all')
 
 n_classes=3
-dimention=10
 batch_size=16
 n_epoch=530
 
@@ -33,7 +33,8 @@ x_shared=theano.shared(np.zeros((batch_size,dimention),dtype=theano.config.float
 y_shared=theano.shared(np.zeros((batch_size,1),dtype=np.int32),borrow=True)
 
 l_in = lasagne.layers.InputLayer(shape=(None, 1,dimention))
-l_theta = lasagne.layers.DenseLayer(l_in,3,W=lasagne.init.Normal(std=0.1))
+l_in1=lasagne.layers.DenseLayer(l_in,30,W=lasagne.init.Normal(std=0.1),nonlinearity=lasagne.nonlinearities.softmax)
+l_theta = lasagne.layers.DenseLayer(l_in1,3,W=lasagne.init.Normal(std=0.1))
 l_mu=lasagne.layers.NonlinearityLayer(l_theta,nonlinearity=lasagne.nonlinearities.softmax)
 
 probas = lasagne.layers.helper.get_output(l_mu, {l_in: x_shared})
@@ -49,15 +50,21 @@ givens = {
     y: y_shared,
     }
 
-train_model = theano.function([], cost, givens=givens,updates=updates,on_unused_input='ignore',allow_input_downcast=True)
+train_model = theano.function([], [cost,pred], givens=givens,updates=updates,on_unused_input='ignore',allow_input_downcast=True)
 
 for epoch in range(n_epoch):
     batch_total_number=len(xx)/batch_size
-    cost=0
+    cost,error_cout=0,0
     for idx_batch in range(batch_total_number):
         x_batch=xx[idx_batch*batch_size:(idx_batch+1)*batch_size]
-        y_batch=label_binarize(yy[idx_batch*batch_size:(idx_batch+1)*batch_size],range(n_classes))
+        target=yy[idx_batch*batch_size:(idx_batch+1)*batch_size]
+        y_batch=label_binarize(target,range(n_classes))
         x_shared.set_value(x_batch)
         y_shared.set_value(np.int32(y_batch))
-        cost=cost+train_model()
+        cost,pred=train_model()
+        cost+=cost
+
+        count=np.count_nonzero(pred-target)
+        error_cout+=count
     print cost/batch_size
+    print 'error_rate:',1-float(error_cout)/(batch_total_number*batch_size)
