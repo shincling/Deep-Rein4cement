ec3409180cd766ff8ffb6c1aae40f1e7adfe9c01
honeydeer <shiwei2013@ia.ac.cn>
2016-09-28 11:29:13 +0800

    reformat codes

--- Policy_gradient/policy_network_test.py
+++ Policy_gradient/policy_network_test.py
@@ -1,105 +1,115 @@
-#coding=utf8
+# coding=utf8
 import time
 import lasagne
 import numpy as np
 import theano
 import theano.tensor as T
-from sklearn.preprocessing import LabelBinarizer,label_binarize
+from sklearn.preprocessing import LabelBinarizer, label_binarize
+
 
 def get_dataset(dimention):
-    x=np.random.random((10000,dimention))
-    y=np.zeros((10000))
-    for idx,i in enumerate(x):
-        if 0.3<i[0]<0.7:
-            y[idx]=1
-        elif i[0]>=0.7:
-            y[idx]=2
-    return x,y
+    x = 0.1 * np.random.random((10000, dimention))
+    y = np.zeros((10000))
+    for idx, i in enumerate(x):
+        if 0.3 < i[0] < 0.7:
+            y[idx] = 1
+        elif i[0] >= 0.7:
+            y[idx] = 2
+    return x, y
 
-def action_to_vector(x,n_classes):
-    result=np.zeros([x.shape[0],x.shape[1],n_classes])
+
+def action_to_vector(x, n_classes):
+    result = np.zeros([x.shape[0], x.shape[1], n_classes])
     for i in range(x.shape[0]):
         for j in range(x.shape[1]):
-            if x[i,j]==0:
-                result[i,j]=np.array([1.0,0,0])
-            elif x[i,j]==1:
-                result[i,j]=np.array([0,1.0,0])
-            elif x[i,j]==2:
-                result[i,j]=np.array([0,0,1.0])
+            if x[i, j] == 0:
+                result[i, j] = np.array([1.0, 0, 0])
+            elif x[i, j] == 1:
+                result[i, j] = np.array([0, 1.0, 0])
+            elif x[i, j] == 2:
+                result[i, j] = np.array([0, 0, 1.0])
     return result
 
-def reward_count(total_reward,length,discout=0.99):
+
+def reward_count(total_reward, length, discout=0.99):
     '''
     for line in range(total_reward.shape[0]):
         for step in range(length):
             result[line]+=total_reward[line,step]*(discout**step)
     '''
-    discout_list=np.zeros(length)
+    discout_list = np.zeros(length)
     for idx in range(discout_list.shape[0]):
-        step=discout**idx
-        discout_list[idx]=step
-    result=np.dot(total_reward,discout_list)
+        step = discout ** idx
+        discout_list[idx] = step
+    result = np.dot(total_reward, discout_list)
     return result
 
-dimention=10
-xx,yy=get_dataset(dimention)
-yy=np.int32(yy)
-x=T.matrix('x')
-y=T.imatrix('y')
-x1=T.vector('x1')
-x2=T.matrix('all')
 
-n_classes=3
-batch_size=100
-n_epoch=500
-path_lenth=10
-n_paths=5000
-max_norm=100
-lr=0.01
-std=1
-print 'batch_size:{},n_paths:{},std:{},lr:{}'.format(batch_size,n_paths,std,lr)
+dimention = 10
+xx, yy = get_dataset(dimention)
+yy = np.int32(yy)
+x = T.matrix('x')
+y = T.imatrix('y')
+x1 = T.vector('x1')
+x2 = T.matrix('all')
 
-x_shared=theano.shared(np.zeros((batch_size,dimention),dtype=theano.config.floatX),borrow=True)
-y_shared=theano.shared(np.zeros((batch_size,1),dtype=np.int32),borrow=True)
+n_classes = 3
+batch_size = 64
+n_epoch = 500
+path_lenth = 10
+n_paths = 5000
+max_norm = 100
+lr = 0.2
+std = 1
+print 'batch_size:{},n_paths:{},std:{},lr:{}'.format(batch_size, n_paths, std, lr)
 
-l_in = lasagne.layers.InputLayer(shape=(None, 1,dimention))
-l_in1= lasagne.layers.DenseLayer(l_in,dimention,W=lasagne.init.Normal(std=std),nonlinearity=lasagne.nonlinearities.sigmoid)
-l_theta = lasagne.layers.DenseLayer(l_in1,3,W=lasagne.init.Normal(std=std))
-l_mu=lasagne.layers.NonlinearityLayer(l_theta,nonlinearity=lasagne.nonlinearities.softmax)
+x_shared = theano.shared(np.zeros((batch_size, dimention), dtype=theano.config.floatX), borrow=True)
+y_shared = theano.shared(np.zeros((batch_size, 1), dtype=np.int32), borrow=True)
+
+l_in = lasagne.layers.InputLayer(shape=(None, 1, dimention))
+l_in1 = lasagne.layers.DenseLayer(l_in, dimention, W=lasagne.init.Normal(std=std),
+                                  nonlinearity=lasagne.nonlinearities.sigmoid)
+l_theta = lasagne.layers.DenseLayer(l_in1, 3, W=lasagne.init.Normal(std=std))
+l_mu = lasagne.layers.NonlinearityLayer(l_theta, nonlinearity=lasagne.nonlinearities.softmax)
 
 probas = lasagne.layers.helper.get_output(l_mu, {l_in: x_shared})
 givens = {
     x: x_shared,
 }
-output_model = theano.function([],probas, givens=givens,on_unused_input='ignore',allow_input_downcast=True)
+output_model = theano.function([], probas, givens=givens, on_unused_input='ignore', allow_input_downcast=True)
 
 if 1:
-    x_range=T.tensor3()
-    x_action=T.tensor3()
-    x_reward=T.vector()
-    x_range_shared=theano.shared(np.zeros((batch_size,path_lenth,dimention),dtype=theano.config.floatX),borrow=True)
-    x_range_action=theano.shared(np.zeros((batch_size,path_lenth,n_classes),dtype=theano.config.floatX),borrow=True)
-    x_range_reward=theano.shared(np.zeros(batch_size,dtype=theano.config.floatX),borrow=True)
+    x_range = T.tensor3()
+    x_action = T.tensor3()
+    x_reward = T.vector()
+    x_range_shared = theano.shared(np.zeros((batch_size, path_lenth, dimention), dtype=theano.config.floatX),
+                                   borrow=True)
+    x_range_action = theano.shared(np.zeros((batch_size, path_lenth, n_classes), dtype=theano.config.floatX),
+                                   borrow=True)
+    x_range_reward = theano.shared(np.zeros(batch_size, dtype=theano.config.floatX), borrow=True)
 
-    l_range_in = lasagne.layers.InputLayer(shape=(batch_size,path_lenth,dimention))
-    l_range_theta = lasagne.layers.ReshapeLayer(l_range_in,[batch_size*path_lenth,dimention])
-    l_range_in1= lasagne.layers.DenseLayer(l_range_theta,dimention,W=l_in1.W,nonlinearity=lasagne.nonlinearities.sigmoid)
-    l_range_remu = lasagne.layers.DenseLayer(l_range_in1,n_classes,W=l_theta.W,nonlinearity=lasagne.nonlinearities.softmax)
+    l_range_in = lasagne.layers.InputLayer(shape=(batch_size, path_lenth, dimention))
+    l_range_theta = lasagne.layers.ReshapeLayer(l_range_in, [batch_size * path_lenth, dimention])
+    l_range_in1 = lasagne.layers.DenseLayer(l_range_theta, dimention, W=l_in1.W,
+                                            nonlinearity=lasagne.nonlinearities.sigmoid)
+    l_range_remu = lasagne.layers.DenseLayer(l_range_in1, n_classes, W=l_theta.W,
+                                             nonlinearity=lasagne.nonlinearities.softmax)
     # l_range_remu = lasagne.layers.DenseLayer(l_range_theta,n_classes,W=l_theta.W,nonlinearity=lasagne.nonlinearities.softmax)
-    l_range_mu = lasagne.layers.ReshapeLayer(l_range_remu,[batch_size,path_lenth,n_classes])
+    l_range_mu = lasagne.layers.ReshapeLayer(l_range_remu, [batch_size, path_lenth, n_classes])
     probas_range = lasagne.layers.helper.get_output(l_range_mu, {l_range_in: x_range_shared})
-    params=lasagne.layers.helper.get_all_params(l_range_mu,trainable=True)
+    params = lasagne.layers.helper.get_all_params(l_range_mu, trainable=True)
     givens = {
         x_range: x_range_shared,
         x_action: x_range_action,
-        x_reward:x_range_reward
+        x_reward: x_range_reward
     }
-    cost=-T.mean(T.sum(T.sum(T.log(probas_range)*x_action,axis=2),axis=1)*x_reward)
-    grads=T.grad(cost,params)
+    cost = -T.mean(T.sum(T.sum(T.log(probas_range) * x_action, axis=2), axis=1) * x_reward)
+    grads = T.grad(cost, params)
     # scaled_grads = lasagne.updates.total_norm_constraint(grads, max_norm)
-    updates = lasagne.updates.adagrad(grads, params, learning_rate=lr)
+    updates = lasagne.updates.sgd(grads, params, learning_rate=lr)
 
-    output_model_range = theano.function([],[probas_range,cost],givens=givens,updates=updates,on_unused_input='ignore',allow_input_downcast=True)
+    output_model_range = theano.function([], [probas_range, cost], givens=givens, updates=updates,
+                                         on_unused_input='ignore', allow_input_downcast=True)
     # x_range_batch=np.random.rand(batch_size,path_lenth,dimention)
     # x_range_action_batch=np.int32(np.random.randint(0,batch_size,size=[batch_size,path_lenth]))
     # x_range_shared.set_value(x_range_batch)
@@ -117,83 +127,84 @@ if 1:
 # probs=output_model()
 # print probs
 
-def sample_one_path(state,prob):
-    n_action=len(prob)
-    action=np.random.choice([0,1,2],p=prob)
-    if state[0]<0.5:
-        if action ==0:
-            reward=5
-            state[0]+=0.1
-        elif action ==1:
-            reward=2
+def sample_one_path(state, prob):
+    n_action = len(prob)
+    action = np.random.choice([0, 1, 2], p=prob)
+    if state[0] < 0.5:
+        if action == 0:
+            reward = 5
+            state[0] += 0.1
+        elif action == 1:
+            reward = 2
             # state[1]+=0.1
         else:
-            reward=1
+            reward = 1
     else:
-        if action ==0:
-            reward=0
-            state[0]+=0.1
-        elif action ==1:
-            reward=2
+        if action == 0:
+            reward = 0
+            state[0] += 0.1
+        elif action == 1:
+            reward = 2
             # state[1]+=0.1
         else:
-            reward=1
-    return action,state,reward
+            reward = 1
+    return action, state, reward
+
 
 for epoch in range(n_epoch):
-    begin_time=time.time()
-    batch_total_number=len(xx)/batch_size
+    begin_time = time.time()
+    batch_total_number = len(xx) / batch_size
     np.random.shuffle(xx)
     for idx_batch in range(batch_total_number):
-        x_batch=xx[idx_batch*batch_size:(idx_batch+1)*batch_size]
-        #初始化两个循环的参数，state和概率
+        x_batch = xx[idx_batch * batch_size:(idx_batch + 1) * batch_size]
+        # 初始化两个循环的参数，state和概率
         x_shared.set_value(x_batch)
-        probs_sample_0=output_model()
+        probs_sample_0 = output_model()
 
-        tmp_cost,tmp_result=0,0
-        for repeat_time in range(n_paths):#每一个batch都要经过多次重复采样获取不同的道路
-            #但是第一步的初始化状态都是一样的
-            total_state=np.zeros([batch_size,path_lenth,dimention])
-            total_reward=np.zeros([batch_size,path_lenth])
-            total_action=np.zeros([batch_size,path_lenth])
-            total_probs=np.zeros([batch_size,path_lenth,n_classes])
-            total_state[:,0,:]=x_batch
-            total_probs[:,0,:]=probs_sample_0
-            for t in range(path_lenth):#进行了10步
-                for idx,prob in enumerate(total_probs[:,t,:]):#对于batch里的每一个样本
-                    now_state=total_state[idx,t].copy()
-                    action,new_state,reward=sample_one_path(now_state,prob)
-                    #action是这一步采取的动作，state是进入的新的状态，prob四
-                    total_action[idx,t]=action
-                    total_reward[idx,t]=reward
-                    #更新state和概率,下一个循环使用
-                    if t!=path_lenth-1:
-                        total_state[idx,t+1]=new_state
-                    del new_state,now_state
+        tmp_cost, tmp_result = 0, 0
+        for repeat_time in range(n_paths):  # 每一个batch都要经过多次重复采样获取不同的道路
+            # 但是第一步的初始化状态都是一样的
+            total_state = np.zeros([batch_size, path_lenth, dimention])
+            total_reward = np.zeros([batch_size, path_lenth])
+            total_action = np.zeros([batch_size, path_lenth])
+            total_probs = np.zeros([batch_size, path_lenth, n_classes])
+            total_state[:, 0, :] = x_batch
+            total_probs[:, 0, :] = probs_sample_0
+            for t in range(path_lenth):  # 进行了10步
+                for idx, prob in enumerate(total_probs[:, t, :]):  # 对于batch里的每一个样本
+                    now_state = total_state[idx, t].copy()
+                    action, new_state, reward = sample_one_path(now_state, prob)
+                    # action是这一步采取的动作，state是进入的新的状态，prob四
+                    total_action[idx, t] = action
+                    total_reward[idx, t] = reward
+                    # 更新state和概率,下一个循环使用
+                    if t != path_lenth - 1:
+                        total_state[idx, t + 1] = new_state
+                    del new_state, now_state
 
-                if t!=path_lenth-1:
-                    x_shared.set_value(total_state[:,t+1])
-                    total_probs[:,t+1,:]=output_model()
+                if t != path_lenth - 1:
+                    x_shared.set_value(total_state[:, t + 1])
+                    total_probs[:, t + 1, :] = output_model()
 
             x_range_shared.set_value(total_state)
-            x_range_action.set_value(action_to_vector(total_action,n_classes))
-            x_range_reward.set_value(reward_count(total_reward,length=path_lenth,discout=0.99))
-            aver_reward=np.mean(np.sum(np.float32(total_reward),axis=1))
-            _,cost=output_model_range()
-            tmp_cost+=cost
-            tmp_result+=aver_reward
+            x_range_action.set_value(action_to_vector(total_action, n_classes))
+            x_range_reward.set_value(reward_count(total_reward, length=path_lenth, discout=0.99))
+            aver_reward = np.mean(np.sum(np.float32(total_reward), axis=1))
+            _, cost = output_model_range()
+            tmp_cost += cost
+            tmp_result += aver_reward
             # print 'cost:{},average_reward:{}'.format(cost,aver_reward)
             # print _[0]
             # print '\n\n\n'
-        print 'cost:{},average_reward:{}'.format(tmp_cost/n_paths,tmp_result/n_paths)
-        if tmp_result/n_paths>19:
+        print 'cost:{},average_reward:{}'.format(tmp_cost / n_paths, tmp_result / n_paths)
+        if tmp_result / n_paths > 19:
             print total_state[0]
             print total_action[0]
             print _[0]
             print total_reward[0]
             print '\n\n'
 
-    print 'epoch:{},time:{}'.format(epoch,time.time()-begin_time)
+    print 'epoch:{},time:{}'.format(epoch, time.time() - begin_time)
 
 '''
 states=x_batch
