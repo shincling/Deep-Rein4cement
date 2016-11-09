024dce921f471f7bb73e4b605612185e3835906b
shincling <shijing609@163.com>
2016-11-09 10:55:09 +0800

    build the CNN layer of the Image

--- One-shot-PGD/Omniglot/OneShot_Omniglot.py
+++ One-shot-PGD/Omniglot/OneShot_Omniglot.py
@@ -10,6 +10,7 @@ import theano.tensor as T
 from sklearn.preprocessing import LabelBinarizer,label_binarize
 import image
 x_train,y_train,x_test,y_test=image.x_train,image.y_train,image.x_test,image.y_test
+yy_train,yy_test=image.y_train_shuffle,image.y_test_shuffle
 
 def action_to_vector(x, n_classes): #x是bs*path_length
     result = np.zeros([x.shape[0], x.shape[1], n_classes])
@@ -158,7 +159,7 @@ class ChoiceLayer(lasagne.layers.MergeLayer):
         return alpha
 
 class Model:
-    def __init__(self,x_dimension=20,h_dimension=30,n_classes=10,batch_size=32,n_epoch=50,path_length=10,
+    def __init__(self,x_dimension=(20,20),h_dimension=30,n_classes=10,batch_size=32,n_epoch=50,path_length=10,
                  n_paths=1000,max_norm=50,lr=0.05,discount=0.99,update_method='sgd',std=0.5,save_path=str(np.random.randint(100,999)),**kwargs):
         self.h_dim=h_dimension
         self.n_classes=n_classes
@@ -185,27 +186,39 @@ class Model:
         self.build()
 
     def build(self):
-        x_range=T.tensor3()
+        x_range=T.tensor4()
         x_action=T.tensor3()
         x_reward=T.vector()
         x_memory=T.tensor4()
 
-        self.x_range_shared=theano.shared(np.zeros((self.batch_size,self.path_length,self.x_dim),dtype=theano.config.floatX),borrow=True)
+        self.x_range_shared=theano.shared(np.zeros((self.batch_size,self.path_length,self.x_dim[0],self.x_dim[1]),dtype=theano.config.floatX),borrow=True)
         self.x_range_action=theano.shared(np.zeros((self.batch_size,self.path_length,self.n_classes),dtype=theano.config.floatX),borrow=True)
         self.x_range_reward=theano.shared(np.zeros(self.batch_size,dtype=theano.config.floatX),borrow=True)
         self.x_range_memory=theano.shared(np.zeros((self.batch_size,self.path_length,self.n_classes,self.h_dim),dtype=theano.config.floatX),borrow=True)
 
         '''前期的框架模型，主要是得到x到h的映射，以及memory的构建'''
         D1, D2, D3 = lasagne.init.Normal(std=self.std), lasagne.init.Normal(std=self.std), lasagne.init.Normal(std=self.std)
-        l_range_in = lasagne.layers.InputLayer(shape=(self.batch_size,self.path_length,self.x_dim))
-        l_range_flatten = lasagne.layers.ReshapeLayer(l_range_in,[self.batch_size*self.path_length,self.x_dim])
-        # l_range_dense1 = lasagne.layers.DenseLayer(l_range_flatten,self.dimension,W=D1,nonlinearity=lasagne.nonlinearities.sigmoid)
-        l_range_dense2 = lasagne.layers.DenseLayer(l_range_flatten,self.h_dim,W=D2,nonlinearity=lasagne.nonlinearities.sigmoid) #[bs*path_length,dimension]
-        l_range_dense2 = lasagne.layers.DenseLayer(l_range_dense2,self.h_dim,W=D2,nonlinearity=lasagne.nonlinearities.sigmoid) #[bs*path_length,dimension]
+        l_range_in = lasagne.layers.InputLayer(shape=(self.batch_size,self.path_length,self.x_dim[0],self.x_dim[1]))
+        l_range_flatten = lasagne.layers.ReshapeLayer(l_range_in,[self.batch_size*self.path_length,self.x_dim[0],self.x_dim[1]])
+        l_range_conv1=lasagne.layers.Conv2DLayer(l_range_flatten,num_filters=128,filter_size=(3,3),nonlinearity=lasagne.nonlinearities.rectify)
+        l_range_conv1=lasagne.layers.Conv2DLayer(l_range_conv1,num_filters=128,filter_size=(3,3),nonlinearity=lasagne.nonlinearities.rectify)
+        l_pool1=lasagne.layers.MaxPool2DLayer(l_range_conv1,pool_size=(2,2))
+        # l_dropout1=lasagne.layers.DropoutLayer(l_pool1,p=0.2)
+
+
+        l_range_conv2=lasagne.layers.Conv2DLayer(l_pool1,num_filters=128,filter_size=(3,3),nonlinearity=lasagne.nonlinearities.rectify)
+        l_range_conv2=lasagne.layers.Conv2DLayer(l_range_conv2,num_filters=128,filter_size=(3,3),nonlinearity=lasagne.nonlinearities.rectify)
+        l_pool2=lasagne.layers.MaxPool2DLayer(l_range_conv2,pool_size=(2,2))
+        l_dropout2=lasagne.layers.DropoutLayer(l_pool2,p=0.2)
+
+        l_range_dense2 = lasagne.layers.DenseLayer(l_dropout2,1024,W=D1,nonlinearity=lasagne.nonlinearities.rectify) #[bs*path_length,dimension]
+        l_dropout3=lasagne.layers.DropoutLayer(l_range_dense2,p=0.5)
+        l_range_dense2 = lasagne.layers.DenseLayer(l_dropout3,self.h_dim,W=D2,nonlinearity=lasagne.nonlinearities.sigmoid) #[bs*path_length,dimension]
         l_range_dense2_origin=lasagne.layers.ReshapeLayer(l_range_dense2,[self.batch_size,self.path_length,self.h_dim])
         l_range_hidden=lasagne.layers.ReshapeLayer(l_range_dense2,[self.batch_size*self.path_length,1,self.h_dim])
-        if test_mode:
-            l_range_dense2,l_range_dense2_origin=l_range_in,l_range_in
+
+        # if test_mode:
+        #     l_range_dense2,l_range_dense2_origin=l_range_in,l_range_in
         # l_range_mu = lasagne.layers.DenseLayer(l_range_hidden,self.n_slot,W=D2,nonlinearity=lasagne.nonlinearities.softmax)
 
         '''Policy Gradient Methods的模型，主要是从Memory状态得到action的概率'''
@@ -409,7 +422,7 @@ if __name__=='__main__':
     if test_mode:
         parser.add_argument('--x_dimension', type=int, default=10, help='Dimension#')
     else:
-        parser.add_argument('--x_dimension', type=int, default=20, help='Dimension#')
+        parser.add_argument('--x_dimension', type=tuple, default=(20,20), help='Dimension#')
     parser.add_argument('--h_dimension', type=int, default=50, help='Dimension#')
     parser.add_argument('--n_classes', type=int, default=10, help='Task#')
     parser.add_argument('--batch_size', type=int, default=1000, help='Task#')
