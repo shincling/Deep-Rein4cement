b32b91edbbc473ac17e5997baebfd546d2e7eb22
shincling <shijing609@163.com>
2017-07-26 10:39:39 +0800

    print the params

--- One-shot-PGD/Omniglot/OneShot_rotate_cont.py
+++ One-shot-PGD/Omniglot/OneShot_rotate_cont.py
@@ -322,7 +322,8 @@ class Model:
         hidden = lasagne.layers.helper.get_output(l_range_dense2_origin, {l_range_in: x_range,l_range_label:x_label})
         probas_range = lasagne.layers.helper.get_output(l_range_mu, {l_range_in: x_range,l_range_memory_in:x_memory,l_range_label:x_label})
         params=lasagne.layers.helper.get_all_params(l_range_mu,trainable=True)
-        params=[]#相当于只更新最后一个参数，别的不参与更新了
+        print 'params:',params
+        params=params[-1:]#相当于只更新最后一个参数，别的不参与更新了
         # params=params[:]#相当于只更新最后一个参数，别的不参与更新了
         givens = {
             x_range: self.x_range_shared,
