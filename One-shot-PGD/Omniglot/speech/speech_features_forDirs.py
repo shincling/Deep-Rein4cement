527ca5fbb8adb8f6128579823ef381f23dd9f930
shincling <shijing609@163.com>
2017-08-10 16:14:39 +0800

    original version to get features and split it.

---
+++ One-shot-PGD/Omniglot/speech/speech_features_forDirs.py
@@ -0,0 +1,33 @@
+#coding=utf8
+import sys
+from python_speech_features import logfbank
+import scipy.io.wavfile as wav
+import numpy as np
+
+def split_speech(features,length=None):#如果不给第二个参数，就默认等同于跟特征个数一样的
+    list=[]
+    if not length:
+        length=features.shape[1]
+    number=features.shape[0]/length
+    for i in range(number):
+        split=features[i*length:(i+1)*length]
+        list.append(split)
+    return list
+
+
+
+# path=sys.argv[1]
+path='/home/sw/Shin/数据集/多说话人语音数据 - WSJ0/spk01_401/401a010a.wav'
+wav_name=path[path.rfind('/')+1:]
+print 'wav_name:',wav_name
+# output_dir=sys.argv[2]
+output_dir='/home/sw/Shin/Codes/Deep-Rein4cement/One-shot-PGD/Omniglot/speech/dataset'
+output_dir=output_dir if output_dir[-1]=='/' else output_dir+'/'
+print 'output_dir:',output_dir
+
+(rate,sig)=wav.read(path)
+logfbank0015_feat=logfbank(sig,rate,winstep=0.015,nfilt=40)
+feat_list=split_speech(logfbank0015_feat)
+
+# np.save(output_dir+wav_name+'.0015',logfbank0015_feat)
+pass
