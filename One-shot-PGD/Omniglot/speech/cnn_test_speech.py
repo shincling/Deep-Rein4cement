528a1f85f98669637fc3f5045cd2c7cb63d7a0c5
shincling <shijing609@163.com>
2017-08-10 18:19:32 +0800

    begin to cnn with speech.

--- One-shot-PGD/Omniglot/speech/cnn_test_speech.py
+++ One-shot-PGD/Omniglot/speech/cnn_test_speech.py
@@ -11,7 +11,7 @@ from theano import tensor as T
 import lasagne
 from lasagne.nonlinearities import rectify, softmax, very_leaky_rectify,tanh
 from lasagne.updates import nesterov_momentum
-from lasagne.layers import InputLayer, MaxPool2DLayer, Conv2DLayer, DenseLayer, DropoutLayer, helper , BatchNormLayer #batchnorm层噢
+from lasagne.layers import InputLayer, MaxPool2DLayer, Conv2DLayer, DenseLayer, DropoutLayer, helper , batch_norm#batchnorm层噢
 from sklearn.preprocessing import LabelBinarizer,label_binarize
 import speech_features_forDirs as speech
 from tqdm import tqdm
@@ -26,6 +26,7 @@ print 'all labels:',num_labels,'all samples:',num_speechs
 h_dimension=300
 valid_size=num_speechs/10
 size=num_speechs-valid_size
+valid_best=0.95
 
 def floatX(X):
     return np.asarray(X, dtype=theano.config.floatX)
@@ -33,19 +34,21 @@ def floatX(X):
 def lasagne_model():
     l_in = InputLayer(shape=(None, 1)+speechSize)
 
-    # l_in =lasagne.layers.NonlinearityLayer(l_in,lasagne.nonlinearities.tanh)
     l_conv1 = Conv2DLayer(l_in, num_filters = 128, filter_size=(3,3), nonlinearity=rectify)
     l_conv1b = Conv2DLayer(l_conv1, num_filters = 128, filter_size=(3,3), nonlinearity=rectify)
+    l_conv1b = batch_norm(l_conv1b)
     l_pool1 = MaxPool2DLayer(l_conv1b, pool_size=(2,2))
     # l_dropout1 = DropoutLayer(l_pool1, p=0.2)
 
     l_conv2 = Conv2DLayer(l_pool1, num_filters = 256, filter_size=(3,3), nonlinearity=rectify)
     l_conv2b = Conv2DLayer(l_conv2, num_filters = 256, filter_size=(3,3), nonlinearity=rectify)
+    l_conv2b = batch_norm(l_conv2b)
     l_pool2 = MaxPool2DLayer(l_conv2b, pool_size=(2,2))
     # l_dropout2 = DropoutLayer(l_pool2, p=0.2)
 
     l_hidden3 = DenseLayer(l_pool2, num_units = h_dimension, nonlinearity=tanh)
-    # l_dropout3 = DropoutLayer(l_hidden3, p=0.5)
+    l_hidden3 = batch_norm(l_hidden3)
+    # l_dropout3 = DropoutLayer(l_hidden3, p=0.3)
 
     l_hidden4 = DenseLayer(l_hidden3, num_units = h_dimension, nonlinearity=tanh)
     # l_dropout4 = DropoutLayer(l_hidden4, p=0.5)
@@ -57,8 +60,9 @@ def lasagne_model():
 def main():
     # load the training and validation data sets
     # labels=int(0.7*speech.all_count)
-    data=speech.feat_list
+    data=speech.data_dict
     labels=data.keys()
+    assert (PIXELS,PIXELS)==speechSize
     train_X=np.zeros([num_speechs,1,PIXELS,PIXELS])
     train_y=np.zeros([num_speechs,len(labels)])
     i=0
@@ -102,14 +106,14 @@ def main():
     pred_valid = T.argmax(output_valid, axis=1)
 
     # get parameters from network and set up sgd with nesterov momentum to update parameters
-    params = lasagne.layers.get_all_params(output_layer)
-    # updates = nesterov_momentum(loss_train, params, learning_rate=0.03, momentum=0.9)
-    updates =lasagne.updates.sgd(loss_train, params, learning_rate=0.01)
+    params = lasagne.layers.get_all_params(output_layer,trainable=True)
+    updates = nesterov_momentum(loss_train, params, learning_rate=0.03, momentum=0.9)
+    # updates =lasagne.updates.sgd(loss_train, params, learning_rate=0.01)
 
     # set up training and prediction functions
     train = theano.function(inputs=[X, Y], outputs=[loss_train,pred], updates=updates, allow_input_downcast=True)
-    valid = theano.function(inputs=[X, Y], outputs=[loss_valid], allow_input_downcast=True)
-    predict_valid = theano.function(inputs=[X], outputs=[pred_valid], allow_input_downcast=True)
+    valid = theano.function(inputs=[X, Y], outputs=[loss_valid,pred_valid], allow_input_downcast=True)
+    # predict_valid = theano.function(inputs=[X], outputs=[pred_valid], allow_input_downcast=True)
 
     # loop over training functions for however many iterations, print information while training
     train_eval = []
@@ -121,7 +125,7 @@ def main():
         for idx_batch in range (batch_total_number):
             xx_batch = np.float32(train_X[idx_batch * BATCHSIZE:(idx_batch + 1) * BATCHSIZE])
             yy_batch = np.float32(train_y[idx_batch * BATCHSIZE:(idx_batch + 1) * BATCHSIZE])
-               
+
             train_loss ,pred = train(xx_batch,yy_batch)
             count=np.count_nonzero(np.int32(pred ==np.argmax(yy_batch,axis=1)))
             print i,idx_batch,'| Tloss:', train_loss,'| Count:',count,'| Acc:',float(count)/(BATCHSIZE)
@@ -129,18 +133,25 @@ def main():
             print np.argmax(yy_batch,axis=1)
             print "time:",time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
-            if 1 and idx_batch%1==0:
+            if 1 and idx_batch%15==0:
                 acc=0
                 valid_batch_number=len(valid_X)/BATCHSIZE
                 for j in tqdm(range(valid_batch_number)):
-                    x_batch = np.float32(valid_X[idx_batch * BATCHSIZE:(idx_batch + 1) * BATCHSIZE])
-                    y_batch = np.float32(valid_y[idx_batch * BATCHSIZE:(idx_batch + 1) * BATCHSIZE])
-                    valid_loss = valid(x_batch,y_batch)[0]
-                    pred = predict_valid(x_batch)[0]
+                    x_batch = np.float32(valid_X[j* BATCHSIZE:(j+ 1) * BATCHSIZE])
+                    y_batch = np.float32(valid_y[j* BATCHSIZE:(j+ 1) * BATCHSIZE])
+                    print len(x_batch),len(y_batch)
+                    valid_loss,pred = valid(x_batch,y_batch)
+                    # pred = predict_valid(x_batch)[0]
                     acc += np.count_nonzero(np.int32(pred ==np.argmax(y_batch,axis=1)))
                 acc=float(acc)/(valid_batch_number*BATCHSIZE)
                 print 'iter:', i,idx_batch, '| Vloss:',valid_loss,'|Acc:',acc
-
+                if acc>valid_best:
+                    print 'new valid_best:',valid_best,'-->',acc
+                    valid_best=acc
+                    all_params = helper.get_all_param_values(output_layer)
+                    f = gzip.open('speech_params/validbest_cnn_allbatchnorm_{}_{}_{}.pklz'.format(i,valid_best,time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())), 'wb')
+                    pickle.dump(all_params, f)
+                    f.close()
 
         # save weights
         if i%5:
